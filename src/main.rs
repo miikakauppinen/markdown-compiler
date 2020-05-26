ef2c74a106c771b4a8210d08677f790955766a83
Miika Kauppinen <miika.kauppinen@solita.fi>
2020-05-26 09:12:42 +0300

    Basis for file handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
-fn usage() {
-    println!("Da first");
-    println!("Version {}", get_version());
+fn get_title() -> String {
+    let mut title = String::from(env!("CARGO_PKG_NAME"));
+    title.push_str(" (v");
+    title.push_str(env!("CARGO_PKG_VERSION"));
+    title.push_str("), ");
+    title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+    return title;
 }
 
-fn get_version() -> u16 {
-    1000
+fn parse_markdown_file(_filename: &str) {
+    print_short_banner();
+    println!("[ INFO ] Trying to parse {}...", _filename);
+}
+
+fn print_short_banner() {
+    println!("{}", get_title());
+}
+
+fn print_long_banner() {
+    print_short_banner();
+    println!("Written by: {}\nHomepage: {}\nUsage: tinymd <somefile>.md\n", 
+        env!("CARGO_PKG_AUTHORS"), 
+        env!("CARGO_PKG_HOMEPAGE")
+    );
+}
+
+fn usage() {
+    print_long_banner();
 }
 
 fn main() {
-    usage();
+    let args: Vec<String> = std::env::args().collect();
+
+    match args.len() {
+        2 => parse_markdown_file(&args[1]),
+        _ => { 
+            println!("[ ERROR ] Invalid invocation (you done goofed!)");
+            usage();
+        }
+    }
 }
